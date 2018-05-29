c2fd31f4b199f394cf89ebddb1bbd0cdc82f58bb
xy <2061864@qq.com>
2018-05-29 17:45:17 +0800

    change Token json shape

--- src/main/kotlin/cn/staynoob/springsecurityjwt/Token.kt
+++ src/main/kotlin/cn/staynoob/springsecurityjwt/Token.kt
@@ -1,10 +1,8 @@
 package cn.staynoob.springsecurityjwt
 
-import com.fasterxml.jackson.annotation.JsonValue
 import java.time.Instant
 
 data class Token(
-        @get: JsonValue
         val value: String,
         val tokenType: TokenType,
         val claims: JwtClaims
